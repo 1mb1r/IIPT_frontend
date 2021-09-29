def212a8e3edc11528001746c571fcb23dfb052c
a.titovsky <a.titovsky@dunice.net>
2021-09-29 11:32:06 +0300

    news filtres and search created

---
+++ frontend/src/components/news/NewsComponent.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Card, Avatar } from 'antd';
+import 'antd/dist/antd.css';
+
+const NewsComponent = ({
+  id, title, author, content, tags, image,
+}) => {
+  const { Meta } = Card;
+  return (
+    <div className="news-cards__card card" key={id}>
+      <Link to={`/news/${id}`}>
+        <Card
+          style={{ width: 300 }}
+          cover={(
+            <img
+              alt={title}
+              src={image}
+            />
+              )}
+        >
+          <Meta
+            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+            title={title}
+            description={content}
+          />
+          <Meta
+            title={author}
+            description={tags}
+          />
+        </Card>
+      </Link>
+    </div>
+  );
+};
+
+export default NewsComponent;
+
+NewsComponent.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
--- frontend/src/pages/AllNewsPage.jsx
+++ frontend/src/pages/AllNewsPage.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { setNews } from '../redux/actions/NewsActions';
-import NewsList from '../components/news/NewsList';
+import NewsComponent from '../components/news/NewsComponent';
+import './AllNewsPage.css';
 
 const AllNewsPage = () => {
   const dispatch = useDispatch();
@@ -11,10 +12,58 @@ const AllNewsPage = () => {
     dispatch(setNews());
   }, [dispatch]);
 
+  const news = useSelector((state) => state.allNews.news);
+  const [searchParam, setSearchParam] = useState('');
+  const [filterParam, setFilterParam] = useState('all');
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    if (filterParam === 'all') {
+      const result = news.filter((item) => item.title.toLowerCase().includes(searchParam)
+      || item.author.toLowerCase().includes(searchParam)
+      || item.tags.includes(searchParam));
+      setSearchResult(result);
+    } else {
+      const result = news.filter((item) => item[filterParam].toLowerCase().includes(searchParam));
+      setSearchResult(result);
+    }
+  }, [searchParam, news, filterParam]);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSearchParam(value.toLowerCase());
+  };
+
+  const handleChangeFilter = (event) => {
+    const { value } = event.target;
+    setFilterParam(value);
+  };
+
   return (
-    <div>
+    <div className="app__all-news all-news">
       {error && <Alert message={error} type="error" />}
-      <NewsList />
+      <select
+        className="all-news__selector selector"
+        defaultValue="all"
+        onChange={handleChangeFilter}
+      >
+        <option className="selector__all" value="all">by all</option>
+        <option className="selector__author" value="author">by authors</option>
+        <option className="selector__tags" value="tags">by tags</option>
+      </select>
+      <input className="all-news__search" onChange={handleChange} type="search" />
+      <div className="all-news__news-cards news-cards">
+        {searchResult.map((el) => (
+          <NewsComponent
+            id={el.id}
+            title={el.title}
+            content={el.content}
+            tags={el.tags}
+            author={el.author}
+            image={el.image}
+          />
+        ))}
+      </div>
     </div>
   );
 };
