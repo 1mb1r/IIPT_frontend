a50b96adae5df8bc25dbae09b6ab2df5c1e62967
a.titovsky <a.titovsky@dunice.net>
2021-09-30 12:11:17 +0300

    added pagination

--- frontend/src/pages/AllNewsPage.jsx
+++ frontend/src/pages/AllNewsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Alert } from 'antd';
+import { Alert, Pagination } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { setNews } from '../redux/actions/NewsActions';
 import NewsComponent from '../components/news/NewsComponent';
@@ -11,15 +11,15 @@ const AllNewsPage = () => {
   useEffect(() => {
     dispatch(setNews());
   }, [dispatch]);
-
-  const allFiltersOptions = ['all', 'tags', 'authors'];
+  const newsPerPage = 1;
+  const allFiltersOptions = ['all', 'tag', 'author'];
   const news = useSelector((state) => state.allNews.news);
+  const [currentPage, setCurrentPage] = useState(1);
   const [searchState, setSearchState] = useState('');
   const [filterState, setFilterState] = useState('all');
   const [searchResult, setSearchResult] = useState([]);
-
+  const [totalPages, setTotalPages] = useState(0);
   const normalizeStringAndSearch = (fieldValue) => fieldValue.toLowerCase().replaceAll('ё', 'е').includes(searchState);
-
   const searchNewsByFilter = () => {
     let result = [];
     if (filterState === 'all') {
@@ -27,11 +27,13 @@ const AllNewsPage = () => {
     } else {
       result = news.filter((item) => normalizeStringAndSearch(item[filterState]));
     }
+    setTotalPages(result.length / newsPerPage);
     return result;
   };
 
   useEffect(() => {
     setSearchResult(searchNewsByFilter());
+    setCurrentPage(1);
   }, [searchState, news, filterState]);
 
   const handleChange = (event) => {
@@ -58,18 +60,25 @@ const AllNewsPage = () => {
       </select>
       <input className="all-news__search" onChange={handleChange} type="search" />
       <div className="all-news__news-cards news-cards">
-        {searchResult.map((el) => (
-          <NewsComponent
-            key={el.id}
-            id={el.id}
-            title={el.title}
-            content={el.content}
-            tags={el.tag}
-            author={el.author}
-            image={el.image}
-          />
+        {searchResult.slice((currentPage - 1) * newsPerPage,
+          currentPage * newsPerPage).map((el) => (
+            <NewsComponent
+              key={el.id}
+              id={el.id}
+              title={el.title}
+              content={el.content}
+              tags={el.tag}
+              author={String(el.author)}
+              image={el.image}
+            />
         ))}
       </div>
+      <Pagination
+        onChange={(page) => setCurrentPage(page)}
+        total={totalPages}
+        current={currentPage}
+        pageSize={1}
+      />
     </div>
   );
 };
